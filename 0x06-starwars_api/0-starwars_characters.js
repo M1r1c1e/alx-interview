48ca0c5c255940ce4c79bf4053e21a4a3f6babaf
M1r1c1e <andersonmiracle@gmail.com>
2024-08-09 00:46:45 +0000

    interview question

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,33 @@
 #!/usr/bin/node
 
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+function fetchCharacterData (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
   });
 }
+
+function fetchAndPrintCharacterNames (movieUrl) {
+  request(movieUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const characterUrls = movieData.characters;
+
+      const promises = characterUrls.map(fetchCharacterData);
+
+      Promise.all(promises)
+        .then(characterNames => {
+          characterNames.forEach(name => console.log(name));
+        });
+    }
+  });
+}
+
+fetchAndPrintCharacterNames(movieUrl);
